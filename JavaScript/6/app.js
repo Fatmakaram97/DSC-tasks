3c17b4081c9461d8e4521b83c568089b54e6d1dc
Fatmakaram97 <35669960+Fatmakaram97@users.noreply.github.com>
2021-01-28 21:15:09 +0200

    modify the count down events

--- JavaScript/6/app.js
+++ JavaScript/6/app.js
@@ -1,69 +1,81 @@
+// Get the elements using DOM from the HTML file
 let $eventDateAndTime = document.getElementById('eventDateAndTime');
 let $timerDisplay = document.getElementById('timer');
 let $inputEvent = document.querySelector('#allEvents ul');
-let eventItem;
-
+let dateElement,listItem,cancelBtn;
 
 //const date = dateTimeNow.getFullYear()+'-'+(dateTimeNow.getMonth()+1)+'-'+dateTimeNow.getDate();
 //var time = dateTimeNow.getHours() + ":" + dateTimeNow.getMinutes() + ":" + dateTimeNow.getSeconds();
 // console.log(date);
 // console.log(time);
 
+// export the function to be used when it is imported
 export const updateClock = ($eventTitle = 'Your Event') => {
-  let $inputEventItem = document.querySelector('#allEvents ul li');
-
-  var cancelBtn = document.createElement("button");
-  var cancel = document.createTextNode("\u00D7");
-  cancelBtn.className = "cancel";
-  cancelBtn.appendChild(cancel);
-  cancelBtn.classList.add("cancelbutton");
-
-  // create the list item by the input value
-  let listItem = document.createElement("li");
-  let dateElement = document.createElement("span");
-
+  // get time that insert by the user from the input
+  let dateInput = new Date($eventDateAndTime.value)
   let time1 = new Date($eventDateAndTime.value).getTime();
+  let dateTimeNow = new Date();
+  // check if the time is old else create a new event
+  if(dateInput<dateTimeNow){
+    alert("Please enter a valid time for the event.")
+  }
 
+  else{
+    let $inputEventItem = document.querySelector('#allEvents ul li');
+    // create the cancel button for each list item
+    cancelBtn = document.createElement("button");
+    let cancel = document.createTextNode("\u00D7");
+    cancelBtn.className = "cancel";
+    cancelBtn.appendChild(cancel);
+    // add the class for the ancel button
+    cancelBtn.classList.add("cancelbutton");
+
+    // create the list item by the input event
+    listItem = document.createElement("li");
+    // create the place in the list item to insert in it the count down for the event
+    dateElement = document.createElement("span");
+
+
+  // create the update count down timer that update it self after 1 sec
   const timeinterval = setInterval(function () {
-  const dateTimeNow = new Date();
-  const time = dateTimeNow.getTime();
-  // if(time1<time){
-  //   alert("Please enter a valid time for the event.")
-  // }
-  const timeDiff = time1 - time;
-  let seconds,minutes,hours,days;
-  [seconds, minutes, hours, days ] = [Math.floor( (timeDiff/1000) % 60 )
-  ,Math.floor( (timeDiff/1000/60) % 60)
-  ,Math.floor( (timeDiff/(1000*60*60)) % 24)
-  ,Math.floor( timeDiff/(1000*60*60*24) )];
+      // get the time of the moment that function start at
+    let timeNow = new Date();
+    const time = timeNow.getTime();
 
-    if (seconds === 0 && minutes === 0 && hours === 0 && days === 0){
-      alert(`Event: ${$eventTitle} starts now! Go ahead.`)
-    }
-    else if (seconds < 0) {
-      $inputEventItem.innerHTML = `Event: ${$eventTitle} has been started.`;
-    }
-    else {
-      // eventItem = document.createTextNode(`The event: ${$eventTitle} is in ${days} days,
-      // ${hours} hours, ${minutes} minutes, and ${seconds} seconds`);
-      // console.log(eventItem);
-    dateElement .innerHTML = `Event: ${$eventTitle} is in ${days} days,
-    ${hours} h, ${minutes} m, and ${seconds} s.`;
-    }
-}
-  ,1000);
+    // get the difference between each two times and make the calculations
+    // to get the remaining days, hours, minutes, and seconds
+    const timeDiff = time1 - time;
+    let seconds,minutes,hours,days;
+    [seconds, minutes, hours, days ] = [Math.floor( (timeDiff/1000) % 60 )
+    ,Math.floor( (timeDiff/1000/60) % 60)
+    ,Math.floor( (timeDiff/(1000*60*60)) % 24)
+    ,Math.floor( timeDiff/(1000*60*60*24) )];
+    // create the conditions to check the remaining time
+      if (seconds === 0 && minutes === 0 && hours === 0 && days === 0){
+        alert(`Event: ${$eventTitle} starts now! Go ahead.`)
+      }
+      else if (seconds < 0) {
+        dateElement.innerHTML = `Event: ${$eventTitle} has been started.`;
+      }
+      else {
+      dateElement.innerHTML = `Event: ${$eventTitle} is in ${days} days,
+      ${hours} h, ${minutes} m, and ${seconds} s.`;
+      }
+},1000);
+  // append the data calculated and the cancel button and append all to the list
   listItem.append(dateElement,cancelBtn );
   $inputEvent.append(listItem)
-
-  var cancelbtns = document.getElementsByClassName("cancel");
+}
+  // get the cancel buttons array and call back the cancelation function
+  let cancelbtns = document.getElementsByClassName("cancel");
   cancelation(cancelbtns);
 }
 
+// create function to cancel the selected list item
 function cancelation(cancelbtns) {
   for (var i = 0; i < cancelbtns.length; i++) {
     cancelbtns[i].addEventListener("click", function() {
     this.parentElement.style.display = 'none';
-
   });
 }
 }
--- JavaScript/6/main.js
+++ JavaScript/6/main.js
@@ -1,20 +1,22 @@
+// import all things from app script file
 import * as importedFrom from './app.js'
 
+// Get the elements using DOM from the HTML file
 let $doneBtn = document.getElementById('insert');
 let $inputEvent = document.querySelector('#allEvents ul');
 let $clear = document.querySelector('.clear i');
 
-
+// make function for the start button when it clicks
 $doneBtn.addEventListener("click",function (e) {
-  // console.log(seconds,minutes,hours,days);
-  // create the cancel button
+  // Get the title name using DOM from the HTML file
   let $eventTitle = document.getElementById('eventTitle').value;
   // console.log($eventTitle);
+  // call the function from the app script
   importedFrom.updateClock($eventTitle);
-
 })
 
 // Add the restart function
 $clear.addEventListener("click",function (e){
+  // clear the list
   $inputEvent.innerHTML = '';
 })
